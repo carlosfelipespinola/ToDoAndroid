2442563b4321a4b45af2c9bc05f62f90988268df
Carlos Felipe <carlosfelipespiinola@gmail.com>
2019-05-01 15:27:55 -0300

    Toolbar adicionada

--- app/src/main/java/com/example/letsdo/MainActivity.kt
+++ app/src/main/java/com/example/letsdo/MainActivity.kt
@@ -2,17 +2,21 @@ package com.example.letsdo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupActionBarWithNavController
-import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         val navController = findNavController(R.id.nav_host_fragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
     }
@@ -21,4 +25,5 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp()
     }
+
 }
