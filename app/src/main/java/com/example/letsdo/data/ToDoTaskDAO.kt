932e909cb4f1fe6d5c980cc7676c1ebdf467ed8f
Carlos Felipe <carlosfelipespiinola@gmail.com>
2019-05-03 13:02:53 -0300

    metodo de busca por uid adicionado a ToDOTaskDAO; correção de tipo de dados que estava errado;

--- app/src/main/java/com/example/letsdo/data/ToDoTaskDAO.kt
+++ app/src/main/java/com/example/letsdo/data/ToDoTaskDAO.kt
@@ -9,11 +9,14 @@ interface ToDoTaskDAO {
     fun insert(toDoTask: ToDoTask): Long
 
     @Update
-    fun update(toDoTask: ToDo)
+    fun update(toDoTask: ToDoTask)
 
     @Delete
-    fun delete(toDoTask: ToDo)
+    fun delete(toDoTask: ToDoTask)
 
     @Query("select * from to_do_task_table where to_do_uid = :toDoUid")
     fun getAllTasksFromToDo(toDoUid: Long): LiveData<List<ToDoTask>>
+
+    @Query("SELECT * FROM to_do_task_table WHERE uid IS :uid")
+    fun getByUid(uid: Long): LiveData<ToDoTask>
 }
